57283c2d628eb587e18c396bac28ad164fc3fd9d
Chandra <chandrashekar.suryad@uleth.ca>
2023-06-20 11:53:52 -0600

    removed UI class and made more and good comments on the classes

--- lstm_class.py
+++ lstm_class.py
@@ -9,20 +9,49 @@ class lstm_class:
         np.random.seed(10)
         self.model = self.build_model()
 
-
     def create_lstm_layer(self, inputs):
+        """
+        Creates an LSTM layer with 150 units.
+
+        Args:
+            inputs: Input tensor to the LSTM layer.
+
+        Returns:
+            LSTM layer.
+        """
         return LSTM(150, name='first_layer')(inputs)
 
-
     def create_dense_layer(self, inputs):
+        """
+        Creates a dense layer with 1 unit.
+
+        Args:
+            inputs: Input tensor to the dense layer.
+
+        Returns:
+            Dense layer.
+        """
         return Dense(1, name='dense_layer')(inputs)
 
-
     def create_output_layer(self, inputs):
+        """
+        Creates an output layer with linear activation.
+
+        Args:
+            inputs: Input tensor to the output layer.
+
+        Returns:
+            Output layer.
+        """
         return Activation('linear', name='output')(inputs)
 
-
     def build_model(self):
+        """
+        Builds the LSTM model.
+
+        Returns:
+            Compiled LSTM model.
+        """
         lstm_input = Input(shape=(self.backcandles, 8), name='lstm_input')
         inputs = self.create_lstm_layer(lstm_input)
         inputs = self.create_dense_layer(inputs)
@@ -32,20 +61,45 @@ class lstm_class:
         model.compile(optimizer=adam, loss='mse')
         return model
 
-   
     def train(self, X_train, y_train, batch_size=15, epochs=30, validation_split=0.1):
+        """
+        Trains the LSTM model.
+
+        Args:
+            X_train: Training data (input sequences).
+            y_train: Target data.
+            batch_size: Number of samples per gradient update.
+            epochs: Number of epochs to train the model.
+            validation_split: Fraction of training data to be used for validation.
+
+        Returns:
+            None.
+        """
         self.X_train = X_train
         self.model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, shuffle=True, validation_split=validation_split)
 
     def predict(self):
+        """
+        Performs predictions using the trained model.
+
+        Returns:
+            Predicted values.
+        """
         self.y_pred = self.model.predict(self.X_train)
-        
         return self.y_pred
 
     def get_trade_signal(self, prediction, threshold=0.5):
-     if prediction >= threshold:
-        return 1
-     else:
-        return 0
+        """
+        Determines the trade signal based on the predicted value and a threshold.
 
+        Args:
+            prediction: Predicted value.
+            threshold: Threshold value for classifying the trade signal.
 
+        Returns:
+            Trade signal (1 for buy, 0 for sell).
+        """
+        if prediction >= threshold:
+            return 1
+        else:
+            return 0
--- stock_data.py
+++ stock_data.py
@@ -1,74 +1,113 @@
 import yfinance as yf
 import pandas_ta as ta
 import numpy as np
-
 from sklearn.preprocessing import MinMaxScaler
-# Data Extractor Class
-# Takes a Stock, Start date and End Date.
 
 class stock_data:
-    
-    def __init__(self,stock_name, start_date, end_date):
+    def __init__(self, stock_name, start_date, end_date):
+        """
+        Initializes the stock_data class with the stock name, start date, and end date.
+
+        Args:
+            stock_name: Name of the stock.
+            start_date: Start date for retrieving stock data.
+            end_date: End date for retrieving stock data.
+        """
         self.name = stock_name
         self.start = start_date
         self.end = end_date
-        self.data = yf.download(tickers = self.name, start = self.start,end=self.end)
+        self.data = yf.download(tickers=self.name, start=self.start, end=self.end)
 
     def set_start_date(self, start_date):
+        """
+        Sets the start date for retrieving stock data.
+
+        Args:
+            start_date: Start date for retrieving stock data.
+
+        Returns:
+            None.
+        """
         self.start = start_date
 
     def set_end_date(self, end_date):
-       self.end = end_date
-        
+        """
+        Sets the end date for retrieving stock data.
+
+        Args:
+            end_date: End date for retrieving stock data.
+
+        Returns:
+            None.
+        """
+        self.end = end_date
+
     def get_data(self):
+        """
+        Returns the downloaded stock data.
+
+        Returns:
+            Downloaded stock data.
+        """
         return self.data
 
-    
-    #Data Scaler using mix max scaling
     def min_max_scaler(self):
-     scale = MinMaxScaler(feature_range=(0,1))
-     self.scaled_arr = scale.fit_transform(self.data)
-     return self.scaled_arr
+        """
+        Performs Min-Max scaling on the stock data.
 
-    #Runs Technical indicators on the data provided
+        Returns:
+            Scaled stock data.
+        """
+        scale = MinMaxScaler(feature_range=(0, 1))
+        self.scaled_arr = scale.fit_transform(self.data)
+        return self.scaled_arr
+
     def indicators(self):
-        #Relative Strength Index Indicator
-        self.data['RSI'] = ta.rsi(self.data.Close, length = 15)
-        #Exponetial Moving Average Indicator
-        
-        #Fast Moving Average
-        self.data['EMAF'] = ta.ema(self.data.Close,length = 25)
-        #Medium Moving Average
-        self.data['EMAM'] = ta.ema(self.data.Close,length = 110)
-        #Slow Moving Average
-        self.data['EMAS'] = ta.ema(self.data.Close,length = 175)
-    
-    #Drop unwated data columns
+        """
+        Runs technical indicators on the stock data.
+
+        Returns:
+            None.
+        """
+        self.data['RSI'] = ta.rsi(self.data.Close, length=15)
+        self.data['EMAF'] = ta.ema(self.data.Close, length=25)
+        self.data['EMAM'] = ta.ema(self.data.Close, length=110)
+        self.data['EMAS'] = ta.ema(self.data.Close, length=175)
+
     def filter_data(self):
+        """
+        Filters and preprocesses the stock data for training.
+
+        Returns:
+            Preprocessed and filtered stock data.
+        """
         self.data['Next Target Close'] = self.data['Adj Close'].shift(-1)
         self.data.dropna(inplace=True)
-        self.data.reset_index(inplace = True)
-        self.data.drop(['Volume', 'Close','Date'],axis = 1,inplace = True)
+        self.data.reset_index(inplace=True)
+        self.data.drop(['Volume', 'Close', 'Date'], axis=1, inplace=True)
         return self.min_max_scaler()
 
-    #Prepoccess Data to be sent to the LSTM network.
     def preprocess_data(self):
-      self.back_candles = 10
-      self.X = []
+        """
+        Preprocesses the data to be sent to the LSTM network.
 
-      for j in range(8):
-        X_column = []
+        Returns:
+            True if preprocessing is successful.
+        """
+        self.back_candles = 10
+        self.X = []
 
-        for i in range(self.back_candles, self.scaled_arr.shape[0]):
-            X_column.append(self.scaled_arr[i - self.back_candles:i, j])
+        for j in range(8):
+            X_column = []
+            for i in range(self.back_candles, self.scaled_arr.shape[0]):
+                X_column.append(self.scaled_arr[i - self.back_candles:i, j])
+            self.X.append(X_column)
 
-        self.X.append(X_column)
+        self.X = np.moveaxis(self.X, [0], [2])
 
-      self.X = np.moveaxis(self.X, [0], [2])
+        yi = np.array(self.scaled_arr[self.back_candles:, -1])
+        self.y = np.reshape(yi, (len(yi), 1))
 
-      yi = np.array(self.scaled_arr[self.back_candles:, -1])
-      self.y = np.reshape(yi, (len(yi), 1))
+        self.X, self.y = np.array(self.X), np.array(self.y)
 
-      self.X, self.y = np.array(self.X), np.array(self.y)
-
-      return True
+        return True
